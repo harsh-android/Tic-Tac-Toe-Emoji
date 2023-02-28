495997de164014032c619bb79aef8eb03877ed44
somilr <ribadiyasomil@email.com>
2023-02-28 17:03:17 +0530

    Commit 2

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.tictactoe
 
+import android.annotation.SuppressLint
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.example.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -29,8 +36,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var anim = AnimationUtils.loadAnimation(this,R.anim.zoom)
+        binding.lnrMain.startAnimation(anim)
+
+        binding.txtReset.setOnClickListener {
+            resetGame()
+        }
+
         binding.i1.setOnClickListener {
-            c1 = addImage(c1,binding.i1)
+            c1 = addImage(c1, binding.i1)
             checkWin()
         }
 
@@ -79,13 +93,15 @@ class MainActivity : AppCompatActivity() {
     private fun addImage(c: Int, i1: ImageView): Int {
         var cc = c
         if (cc == 0) {
-
+            var zoomAnim = AnimationUtils.loadAnimation(this,R.anim.zoom2)
             if (check % 2 == 0) {
                 i1.setImageResource(round)
+                i1.startAnimation(zoomAnim)
                 cc = 1
 
             } else {
                 i1.setImageResource(cross)
+                i1.startAnimation(zoomAnim)
                 cc = 2
             }
 
@@ -96,34 +112,84 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkWin() {
 
-        if(c1==c2 && c2==c3){
+        if (c1 == c2 && c2 == c3) {
             winGame(c1)
-        } else if (c4==c5 && c5==c6) {
+        } else if (c4 == c5 && c5 == c6) {
             winGame(c4)
-        } else if (c7==c8 && c8==c9) {
+        } else if (c7 == c8 && c8 == c9) {
             winGame(c7)
-        } else if (c1==c4 && c4==c7) {
+        } else if (c1 == c4 && c4 == c7) {
             winGame(c1)
-        } else if (c2==c5 && c5==c8) {
+        } else if (c2 == c5 && c5 == c8) {
             winGame(c2)
-        } else if (c3==c6 && c6==c9) {
+        } else if (c3 == c6 && c6 == c9) {
             winGame(c3)
-        } else if (c1==c5 && c5==c9) {
+        } else if (c1 == c5 && c5 == c9) {
             winGame(c1)
-        } else if (c3==c5 && c5==c7) {
+        } else if (c3 == c5 && c5 == c7) {
             winGame(c3)
+        }
+
+        if (c1 != 0 && c2 != 0 && c3 != 0 && c4 != 0 && c5 != 0 && c6 != 0 && c7 != 0 && c8 != 0 && c9 != 0) {
+            binding.txtReset.visibility = View.VISIBLE
         }
 
     }
 
+    @SuppressLint("ResourceAsColor")
     private fun winGame(c: Int) {
 
-        if (c==1) {
-            Log.e(TAG, "winGame: WIN ROUND")
-        }else if (c==2) {
-            Log.e(TAG, "winGame: WIN CROSS")
+        var dialog = Dialog(this)
+        dialog.setContentView(R.layout.win_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(android.R.color.transparent))
+        dialog.setCancelable(false)
+        var cardWin = dialog.findViewById<CardView>(R.id.cardWin)
+        var imgIcon = dialog.findViewById<ImageView>(R.id.imgIcon)
+        var txtReset = dialog.findViewById<TextView>(R.id.txtReset)
+        var zoomAnim = AnimationUtils.loadAnimation(this,R.anim.move_up)
+
+        if (c == 1) {
+            imgIcon.setImageResource(round)
+            dialog.show()
+            cardWin.startAnimation(zoomAnim)
+        } else if (c == 2) {
+            imgIcon.setImageResource(cross)
+            dialog.show()
+            cardWin.startAnimation(zoomAnim)
         }
 
+
+        txtReset.setOnClickListener {
+
+            resetGame()
+            dialog.dismiss()
+
+        }
+
+
+    }
+
+    private fun resetGame() {
+        c1 = 0
+        c2 = 0
+        c3 = 0
+        c4 = 0
+        c5 = 0
+        c6 = 0
+        c7 = 0
+        c8 = 0
+        c9 = 0
+        check = 0
+        binding.i1.setImageResource(0)
+        binding.i2.setImageResource(0)
+        binding.i3.setImageResource(0)
+        binding.i4.setImageResource(0)
+        binding.i5.setImageResource(0)
+        binding.i6.setImageResource(0)
+        binding.i7.setImageResource(0)
+        binding.i8.setImageResource(0)
+        binding.i9.setImageResource(0)
+        binding.txtReset.visibility = View.GONE
     }
 
 
